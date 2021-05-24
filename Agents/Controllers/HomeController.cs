ff92a11ace7ce7994587ad5ea9e34fe0f0f12787
John McNay <John.McNay@improving.com>
2021-05-24 08:43:43 -0500

    trying to make it functional still

--- Agents/Controllers/HomeController.cs
+++ Agents/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Data;
 using Agents.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Agents.Controllers
 {
@@ -18,9 +19,58 @@ namespace Agents.Controllers
             _logger = logger;
         }
 
+        // TODO: This is not where this belongs! We'll continue refactoring and put it elsewhere later.
+        private List<Agents> AllAgentData()
+        {
+            var agents = new List<Agents>();
+
+            //NOTE: DO NOT EVER HARDCODE YOUR CONNECTION STRING IN CODE LIKE THIS
+            using (var conn = new SqlConnection("Server=.;Database=AgentDb;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "SELECT * FROM Agents";
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var name = reader["name"].ToString();
+
+                    agents.Add(new Agents
+                    {
+                        name = name
+                    });
+                }
+            }
+
+            return agents;
+        }
+
         public IActionResult Index()
         {
+            var agents = AllAgentData();
+
+            var vm = new HomeViewModel();
+            vm.Message = "Look at these wonderful books!";
+            vm.Agents = agents;
+
+            return View(vm);
+        }
+
+        public IActionResult AgentsWithAjax()
+        {
             return View();
+        }
+
+        public IActionResult AgentData()
+        {
+            var books = AllAgentData();
+
+            return Json(books);
         }
 
         public IActionResult Privacy()
---
+++ Agents/Models/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agent.Models
+{
+    public class Agent
+    {
+        public string agentCode { get; set; }
+
+        public string name { get; set; }
+
+        public string workingArea { get; set; }
+
+        public float commission { get; set; }
+
+        public string phoneNumber { get; set; }
+        public int id { get; set; }
+    }
+}
